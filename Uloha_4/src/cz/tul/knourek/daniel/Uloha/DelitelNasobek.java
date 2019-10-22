99a2102b49556776abefb55629d601ac3c86d1dc
DanielKnourek <dev.danielknourek@gmail.com>
2019-10-22 13:03:55 +0200

    Uloha 4 Zadaní B Finall

--- Uloha_4/src/cz/tul/knourek/daniel/Uloha/DelitelNasobek.java
+++ Uloha_4/src/cz/tul/knourek/daniel/Uloha/DelitelNasobek.java
@@ -11,8 +11,7 @@ public class DelitelNasobek {
             if ( InValA < 0 ){break; }
 
             Long InValB = input.nextLong();
-            Long Nasobek = InValA;
-            Long Delitel = 1l;
+            Long Nasobek;
             Long ValA = InValA;
             Long ValB = InValB;
 
@@ -21,35 +20,9 @@ public class DelitelNasobek {
                 ValA = ValB;
                 ValB = tmp % ValB;
             }
-            Delitel = ValA;
+            Nasobek = (InValA * InValB)/ValA;
 
-//
-//            Long i = Delitel;
-//            Long SmallerNum = (InValA< InValB) ? InValA:InValB;
-//            Long BiggerNum = (InValA > InValB) ? InValB:InValA;
-//
-//            while (i <= InValA && i <= InValB){
-//                if(InValA % i == 0 && InValB % i == 0){Delitel = i;}
-//                i++;
-//            }
-//
-//            while (Nasobek % SmallerNum != 0){
-////                if(Nasobek % InValA == 0 && Nasobek % InValB == 0){break;}
-//                Nasobek += BiggerNum;
-//            }
-            Nasobek = (InValA * InValB)/Delitel;
-
-            System.out.printf("%d %d%n",Delitel, Nasobek);
+            System.out.printf("%d %d%n",ValA, Nasobek);
         }
-
-//    }
-//    public static int gcd(int a, int b) {
-//        if (a < 1 || b < 1) throw new IllegalArgumentException("a or b is less than 1");
-//        while (b != 0) {
-//            int tmp = a;
-//            a = b;
-//            b = tmp % b;
-//        }
-//        return a;
     }
 }
