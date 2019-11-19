b3f543d13bb72e44398afb44ea24c502f01f6c66
DanielKnourek <dev.danielknourek@gmail.com>
2019-11-19 22:17:30 +0100

    Uloha 4 Pocet Finall

---
+++ Uloha_4/src/cz/tul/knourekdaniel/uloha/Pocet.java
@@ -0,0 +1,35 @@
+package cz.tul.knourekdaniel.uloha;
+
+import java.util.Scanner;
+
+public class Pocet {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        long counter = input.nextLong();
+
+        for (int i = 0; i < counter; i++) {
+            long current = input.nextLong();
+            int CountOfFirst = NumberOfNonPrimitive(current);
+            int TotalCount = 0;
+
+            while((current = input.nextLong()) > 0){
+                int count = NumberOfNonPrimitive(current);
+                if (CountOfFirst == count) {
+                    TotalCount++;
+                }
+
+            }
+            System.out.printf("%d%n",TotalCount);
+        }
+    }
+
+    private static int NumberOfNonPrimitive(long number){
+        int count = 0;
+        for (int i = 2; i <= number/2; i++) {
+            count += (number % i == 0) ? 1 : 0;
+        }
+        return count;
+    }
+}
