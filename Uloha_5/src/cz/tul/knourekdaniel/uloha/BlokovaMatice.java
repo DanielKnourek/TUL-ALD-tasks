085b9762dcdfcfde8c81c4d06fcc2710bef030fc
DanielKnourek <dev.danielknourek@gmail.com>
2019-12-11 00:26:37 +0100

    Uloha 5 Problem 3 done Problem 4 init

---
+++ Uloha_5/src/cz/tul/knourekdaniel/uloha/BlokovaMatice.java
@@ -0,0 +1,12 @@
+package cz.tul.knourekdaniel.uloha;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BlokovaMatice {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
--- Uloha_5/src/cz/tul/knourekdaniel/uloha/SpojovaniPoli.java
+++ Uloha_5/src/cz/tul/knourekdaniel/uloha/SpojovaniPoli.java
@@ -1,11 +1,39 @@
 package cz.tul.knourekdaniel.uloha;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SpojovaniPoli {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int arrayLength;
+        while ((arrayLength = input.nextInt()) > 0){
+            int[] arr1 = loadArray(arrayLength);
+            arrayLength = input.nextInt();
+            int[] arr2 = loadArray(arrayLength);
+            int[] combined = new int[arr1.length + arr2.length];
+            System.arraycopy(arr1,0,combined,0,arr1.length);
+            System.arraycopy(arr2,0,combined,arr1.length,arr2.length);
+            Arrays.sort(combined);
+            show(combined);
+        }
+    }
 
+    private static void show(int[] combined) {
+        StringBuilder out = new StringBuilder();
+        for (int cislo : combined) {
+            out.append(cislo).append(" ");
+        }
+        System.out.println(out);
+    }
+
+    private static int[] loadArray(int arrayLength) {
+        int[] arr = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            arr[i] = input.nextInt();
+        }
+        return arr;
     }
 }
