b3cad93ded801e6523deec3b77db7ce9892bc687
DanielKnourek <dev.danielknourek@gmail.com>
2020-01-03 21:58:59 +0100

    Uloha 6 init Problem 1 init

---
+++ Uloha_6/src/cz/tul/knourekdaniel/uloha/VyhledaniVzorku.java
@@ -0,0 +1,15 @@
+package cz.tul.knourekdaniel.uloha;
+
+import java.util.Scanner;
+
+public class VyhledaniVzorku {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int height;
+        while ((height = input.nextInt()) > 0) {
+
+        }
+    }
+
+}
